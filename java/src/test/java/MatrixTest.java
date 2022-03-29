52d4270339ab6c92174847c2e11785306f35cee9
FiIipson <filip.jasinski@student.tcs.uj.edu.pl>
2022-03-29 13:38:55 +0200

    Some test

---
+++ java/src/test/java/MatrixTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MatrixTest {
+  @Test
+  void SomeTest() {
+    assertEquals(1+1, 2);
+  }
+}
