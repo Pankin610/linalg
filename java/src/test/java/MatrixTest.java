80277f58a5b7a0462f3d30f296e32d4c46bbbd2e
FiIipson <filip.jasinski@student.tcs.uj.edu.pl>
2022-03-29 13:42:57 +0200

    new test

--- java/src/test/java/MatrixTest.java
+++ java/src/test/java/MatrixTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 public class MatrixTest {
   @Test
   void SomeTest() {
-    assertEquals(1+1, 2);
+    assertEquals(1 + 1, 2);
+    assertEquals(0, 2 * 0);
   }
 }
