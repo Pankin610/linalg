78ea1f2c3c0e451a5d62bf0b751b3c163097129d
Daniil-Y <daniil.yurshevich@student.uj.edu.pl>
2022-04-30 14:06:57 +0200

    fixed stupid problems

---
+++ java/src/main/java/linalg/algorithms/LUDecomposition.java
@@ -0,0 +1,71 @@
+package linalg.algorithms;
+
+import linalg.vector.Vector;
+import linalg.matrix.Matrix;
+import linalg.matrix.DenseMatrix;
+import linalg.matrix.DenseMatrixBuilder;
+
+
+public class LUDecomposition{
+
+  Matrix L;      
+  Matrix U;  
+  Matrix Mat;
+
+  LUDecomposition (Matrix Mat) {
+    if(Mat.Cols() != Mat.Rows()){
+      throw new java.lang.IllegalArgumentException();
+    }
+  };
+
+  void ConstructionLU () {
+        
+    final int n = Mat.Rows();
+
+    DenseMatrixBuilder LComponent = new DenseMatrixBuilder(n,n);
+    DenseMatrixBuilder UComponent = new DenseMatrixBuilder(n,n);
+
+    //Doolittle's Method for LU Decompositions
+
+    for(int i=0;i<n;i++){
+      //Calculating U
+      for(int k = i;k < n;k++){
+        int sum= 0;
+        for(int j = 0;j < i;j++){
+          sum+=(LComponent.GetValue(i,j)*UComponent.GetValue(j,k));
+        }
+        UComponent.SetValue(i,k, Mat.ValueAt(i,k) - sum);
+      }
+      //Calculating L
+      for(int k = i;k < n;k++){
+        if(i == k){ 
+          LComponent.SetValue(i,i,1);
+        }else{
+          int sum= 0;
+          for(int j = 0;j < i;j++){
+            sum+=(LComponent.GetValue(k,j)*UComponent.GetValue(j,i));
+          }
+          LComponent.SetValue(k,i, (Mat.ValueAt(k,i) - sum)/UComponent.GetValue(i,i));
+        }
+      }
+    }
+
+    L = LComponent.BuildMatrix();
+    U = UComponent.BuildMatrix();
+  }
+
+  double Determinant(){
+    double res = 1;
+    final int n = Mat.Rows();
+        
+    for(int i = 0;i < n;i++){
+      res*=L.ValueAt(i,i);
+    }
+    for(int i = 0;i < n;i++){
+      res*=U.ValueAt(i,i);
+    }
+    return res;
+  }
+
+  //TODO: Implement function that solves AX = B
+}
