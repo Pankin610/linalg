14cf531e2ad26ff4acda6f12ce3283214e62ed60
Daniil-Y <daniil.yurshevich@student.uj.edu.pl>
2022-05-08 00:06:14 +0200

    modifying gradle build file for javafx and adding sample graphics

---
+++ java/src/main/java/linalg/application/MainMenu.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+
+
+public class MainMenu extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws IOException{   
+    Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));   
+    primaryStage.setTitle("Algorithms");
+    primaryStage.setScene(new Scene(root, 800, 800));
+    primaryStage.show();
+  }
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
---
+++ java/src/main/java/linalg/application/MainMenuController.java
@@ -0,0 +1,27 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class MainMenuController extends MainMenu{
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button start;
+
+    @FXML
+    void initialize() {
+        assert start != null : "fx:id=\"start\" was not injected: check your FXML file 'Untitled'.";
+
+    }
+    @FXML
+    void OnStartClicked(MouseEvent event) {
+      System.out.println("Hello World!");
+    }
+}
