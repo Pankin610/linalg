da56cd5e41f83bc6049fd7875f3be846149de05f
Daniil-Y <72337416+Daniil-Y@users.noreply.github.com>
2022-05-08 00:55:25 +0200

    tabulation fixed

--- java/src/main/java/linalg/application/MainMenuController.java
+++ java/src/main/java/linalg/application/MainMenuController.java
@@ -6,22 +6,24 @@ import javafx.scene.input.MouseEvent;
 
 public class MainMenuController extends MainMenu{
 
-    @FXML
-    private ResourceBundle resources;
+  @FXML
+  private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+  @FXML
+  private URL location;
 
-    @FXML
-    private Button start;
+  @FXML
+  private Button start;
 
-    @FXML
-    void initialize() {
-        assert start != null : "fx:id=\"start\" was not injected: check your FXML file 'Untitled'.";
+  @FXML
+  void initialize() {
+    assert start != null : "fx:id=\"start\" was not injected: check your FXML file 'Untitled'.";
+  }
 
-    }
-    @FXML
-    void OnStartClicked(MouseEvent event) {
-      System.out.println("Hello World!");
-    }
+  @FXML
+  void OnStartClicked(MouseEvent event) {
+    System.out.println("Hello World!");
+  }
+  
 }
+
