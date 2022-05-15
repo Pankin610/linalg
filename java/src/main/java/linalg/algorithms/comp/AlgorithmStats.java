f5f9602660b8da3d8423647d2252473bb7722db4
Pankin610 <pavelsankin610@gmail.com>
2022-05-15 14:48:01 +0200

    getting algo stats

--- java/src/main/java/linalg/algorithms/comp/AlgorithmStats.java
+++ java/src/main/java/linalg/algorithms/comp/AlgorithmStats.java
@@ -1,6 +1,9 @@
 package linalg.algorithm.comp;
 
 import java.time.*;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import linalg.matrix.Matrix;
 
 public class AlgorithmStats {
   
@@ -23,6 +26,20 @@ public class AlgorithmStats {
       .plus(run_duration)
       .dividedBy(num_runs_ + 1);
     num_runs_++;
+  }
+
+  static AlgorithmStats GetAlgoStats(Supplier<Matrix> data, Consumer<Matrix> algo, int num_runs) {
+    Clock clock = Clock.systemDefaultZone();
+    AlgorithmStats stats = new AlgorithmStats();
+
+    for (int i = 0; i < num_runs; i++) {
+      Instant start = clock.instant();
+      algo.accept(data.get());
+      Instant end = clock.instant();
+
+      stats.AddRun(Duration.between(start, end));
+    }
+    return stats;
   }
 
   private int num_runs_;
