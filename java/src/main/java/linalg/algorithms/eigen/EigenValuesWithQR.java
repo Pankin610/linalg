13ea7647bfa87bd8ccbb2cab38ac8d82863cc49c
Pankin610 <pavelsankin610@gmail.com>
2022-05-21 23:25:32 +0200

    eigen with qr

--- java/src/main/java/linalg/algorithms/eigen/EigenValuesWithQR.java
+++ java/src/main/java/linalg/algorithms/eigen/EigenValuesWithQR.java
@@ -1,27 +1,56 @@
 package linalg.algorithms.eigen;
 
-// This class is immutable
-class EifenValuesWithQR implements EigenValuesComputer {
+import linalg.matrix.*;
+import linalg.algorithms.QRDecomposition;
+import linalg.algorithms.eigen.EigenValuesComputer;
+import java.util.Collection;
+import java.util.ArrayList;
 
-  public EigenValuesWithQr(QRDecomposition decomposer, double eps) {
+// This class is immutable
+public class EigenValuesWithQR implements EigenValuesComputer {
+  public EigenValuesWithQR(QRDecomposition decomposer, double eps, int max_iter) {
     decomposer_ = decomposer;
     eps_ = eps;
+    max_iter_ = max_iter;
   }
 
-  public EigenValuesWithQr(QRDecomposition decomposer) {
+  public EigenValuesWithQR(QRDecomposition decomposer) {
     decomposer_ = decomposer;
   }
 
-  // TODO: implement once QRDecompositionBuilder is shipped
   @Override
-  public Collection<Double> GetEigenValues(Matrix mat, int max_iter) {
+  public Collection<Double> GetEigenValues(Matrix mat) {
     Matrix cur_mat = mat;
-    while(max_iter > 0) {
+    int max_iter = 1000;
+    // Until the matrix isn't converged we get its QR and assign it to R*Q
+    while(max_iter != 0) {
+      decomposer_.Decompose(cur_mat);
+      Matrix new_mat = MatrixFactory.DenseMultiply(decomposer_.GetR(), 
+                                            decomposer_.GetQ());
+      boolean changed = false;
+      for (int i = 0; i < cur_mat.Rows() && !changed; i++) {
+        for (int j = 0; j < cur_mat.Cols() && !changed; j++) {
+          if (Math.abs(cur_mat.ValueAt(i, j) - new_mat.ValueAt(i, j)) > eps_) {
+            changed = true;
+          }
+        }
+      }
+
+      if (!changed) {
+        break;
+      }
     }
+    
+    ArrayList<Double> eigen_values = new ArrayList<>();
+    for (int i = 0; i < cur_mat.Cols(); i++) {
+      eigen_values.add(cur_mat.ValueAt(i, i));
+    }
+
+    return eigen_values;
   }
 
   private QRDecomposition decomposer_;
   // -1 means no limit.
-  private int max_num_iter_ = -1;
+  private int max_iter_ = -1;
   double eps_ = 1e-9;
 }
