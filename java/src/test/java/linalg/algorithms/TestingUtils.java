fe8c39eabaf48c3a280771433145e87b9b04915b
Pankin610 <pavelsankin610@gmail.com>
2022-05-22 16:20:35 +0200

    test util

--- java/src/test/java/linalg/algorithms/TestingUtils.java
+++ java/src/test/java/linalg/algorithms/TestingUtils.java
@@ -2,6 +2,7 @@ package linalg.algorithms;
 
 import linalg.matrix.Matrix;
 import org.junit.jupiter.api.Assertions;
+import java.util.Collection;
 
 public class TestingUtils {
   public static void CompareMatrixWithMatrixDelta(Matrix expected, Matrix matrix, double delta) {
@@ -24,5 +25,27 @@ public class TestingUtils {
     for (int i = 1; i < matrix.Rows(); i++)
       for (int j = 0; j < i; j++)
         Assertions.assertEquals(0.0, matrix.ValueAt(i, j), delta);
+  }
+
+  public static boolean IsWithin(
+    Collection<Double> a, Collection<Double> b, double tol) {
+    for (Double x : a) {
+      boolean within = false;
+      for (Double y : b) {
+        if (Math.abs(y - x) < tol) {
+          within = true;
+          break;
+        }
+      }
+      if (!within) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean SameDoubleCollections(
+    Collection<Double> a, Collection<Double> b, double tol) {
+    return IsWithin(a, b, tol) && IsWithin(a, b, tol);
   }
 }
