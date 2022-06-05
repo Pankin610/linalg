aa9011d01883f9fbc8d646844fd1174b5190c95b
Pankin610 <pavelsankin610@gmail.com>
2022-06-05 20:07:56 +0200

    MatrixEntriesByCol

--- java/src/main/java/linalg/matrix/MatrixUtils.java
+++ java/src/main/java/linalg/matrix/MatrixUtils.java
@@ -1,9 +1,23 @@
 package linalg.matrix;
 
 import linalg.matrix.Matrix;
+import java.util.Arrays;
+import java.util.stream.Stream;
+import java.util.ArrayList;
 
 public class MatrixUtils {
   public static boolean IsSquare(Matrix mat) {
     return mat.Rows() == mat.Cols();
+  }
+
+  public static ArrayList<ArrayList<MatrixEntry>> GetMatrixEntriesByCol(Matrix mat) {
+    ArrayList<ArrayList<MatrixEntry>> col_list = new ArrayList<ArrayList<MatrixEntry>>();
+    for (int i = 0; i < mat.Cols(); i++) {
+      col_list.add(new ArrayList<MatrixEntry>());
+    }
+    mat.ForEachEntry(entry -> {
+      col_list.get(entry.Col()).add(entry);
+    });
+    return col_list;
   }
 }
