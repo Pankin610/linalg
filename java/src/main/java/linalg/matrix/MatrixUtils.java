45244f7c436b04e0377196bccbff7933d896527d
Pankin610 <pavelsankin610@gmail.com>
2022-06-05 20:20:30 +0200

    modified sparse multiply

--- java/src/main/java/linalg/matrix/MatrixUtils.java
+++ java/src/main/java/linalg/matrix/MatrixUtils.java
@@ -10,14 +10,14 @@ public class MatrixUtils {
     return mat.Rows() == mat.Cols();
   }
 
-  public static ArrayList<ArrayList<MatrixEntry>> GetMatrixEntriesByCol(Matrix mat) {
-    ArrayList<ArrayList<MatrixEntry>> col_list = new ArrayList<ArrayList<MatrixEntry>>();
-    for (int i = 0; i < mat.Cols(); i++) {
-      col_list.add(new ArrayList<MatrixEntry>());
+  public static ArrayList<ArrayList<MatrixEntry>> GetMatrixEntriesByRow(Matrix mat) {
+    ArrayList<ArrayList<MatrixEntry>> row_list = new ArrayList<ArrayList<MatrixEntry>>();
+    for (int i = 0; i < mat.Rows(); i++) {
+      row_list.add(new ArrayList<MatrixEntry>());
     }
     mat.ForEachEntry(entry -> {
-      col_list.get(entry.Col()).add(entry);
+      row_list.get(entry.Row()).add(entry);
     });
-    return col_list;
+    return row_list;
   }
 }
